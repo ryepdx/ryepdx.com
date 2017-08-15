f2fe87410e12553318597876738cefd7cfbec258
Ryan Casey <ryepdx@gmail.com>
2017-08-15 07:01:55 -0700

    Drop the .html extension in our links, for consistency with the old URL scheme.

--- toc.sh
+++ toc.sh
@@ -1,3 +1,3 @@
-links="$(find . -name "*.html" | sort -r | sed 's/^\.//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' -e 's/.html//g' | while read i; do printf "<li><a href=\"%s\">%s</a></li>\n" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
+links="$(find . -name "*.html" | sort -r | sed -e 's/^\.//g' -e 's/.html//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' | while read i; do printf "<li><a href=\"%s\">%s</a></li>\n" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
 
 printf "$(cat _template.html)" "<h1>Table of Contents</h1><ul>$links</ul>"
