ac648261fb3620db158556f9af6723627b7ad651
Ryan Casey <ryepdx@gmail.com>
2017-08-15 10:24:33 -0700

    Styling the table of contents a bit more.

--- toc.sh
+++ toc.sh
@@ -1,3 +1,4 @@
-links="$(find . -name "*.html" | sort -r | sed -e 's/^\.//g' -e 's/.html//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' | while read i; do printf "<li>%s/%s - <a href=\"%s\">%s</a></li>\n" "$(echo "$i" | cut -f 1)" "$(echo "$i" | cut -f 2)" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
+line_format="<li><span class=\"date\">%s/%s</span><a href=\"%s\">%s</a></li>"
+links="$(find . -name "*.html" | sort -r | sed -e 's/^\.//g' -e 's/.html//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' | while read i; do printf "$line_format\n" "$(echo "$i" | cut -f 1)" "$(echo "$i" | cut -f 2)" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
 
-printf "$(cat _template.html)" "<h1>Table of Contents</h1><ul>$links</ul>"
+printf "$(cat _template.html)" "<h1>Table of Contents</h1><ul class=\"toc\">$links</ul>"
