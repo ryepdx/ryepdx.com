117ac4afac6c0feb784d2c06ec0999c032cf3018
Ryan Casey <ryepdx@gmail.com>
2017-08-15 07:27:48 -0700

    Include year and month in table of contents.

--- toc.sh
+++ toc.sh
@@ -1,3 +1,3 @@
-links="$(find . -name "*.html" | sort -r | sed -e 's/^\.//g' -e 's/.html//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' | while read i; do printf "<li><a href=\"%s\">%s</a></li>\n" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
+links="$(find . -name "*.html" | sort -r | sed -e 's/^\.//g' -e 's/.html//g' | while read x; do echo "$x" | grep "[^-]*-" | sed -e 's/\//\t/g' -e 's/-/ /g' -e 's/[[:graph:]]*/\u&/g' | while read i; do printf "<li>%s/%s - <a href=\"%s\">%s</a></li>\n" "$(echo "$i" | cut -f 1)" "$(echo "$i" | cut -f 2)" "$x" "$(echo "$i" | cut -f 3 | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")"; done; done)"
 
 printf "$(cat _template.html)" "<h1>Table of Contents</h1><ul>$links</ul>"
